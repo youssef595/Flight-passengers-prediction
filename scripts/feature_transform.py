e0c52efcc25e4575a021ae8133320ca6bc11b5c0
youssef595 <youssef.benallal@emines.um6p.ma>
2022-01-06 20:46:32 +0100

    weather_scraper_script

--- scripts/feature_transform.py
+++ scripts/feature_transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
-
+import numpy as np
+from meteostat import Stations, Daily, Point
 from pandas.io.sql import DatabaseError
 
 def dates_encoder(X):
@@ -47,6 +48,30 @@ of American Airlines Group Inc. (AAL) stocks
     merged = pd.merge(X, stocks_data, how="outer", on=["flight_date"])
 
     return merged
+
+def scrap_weather(X, start, end):
+    url="https://raw.githubusercontent.com/ravisurdhar/flight_delays/master/airports.csv"
+    us_airports=pd.read_csv(url)
+    airports = np.unique((np.append(X['from'].unique(), X['to'].unique())))
+    airport_localisation = us_airports[us_airports['IATA_CODE'].isin(airports)]
+    lats = airport_localisation['LATITUDE'].tolist()
+    longs = airport_localisation['LONGITUDE'].tolist()
+    iatas = airports.tolist()
+    list_geo = list(zip(lats,longs))
+    iatas = airports.tolist()
+    list_data=[]
+
+    for i in range(len(list_geo)):
+        point = Point(list_geo[i][0], list_geo[i][1])
+        data = Daily(point, start, end)
+        df = data.fetch()
+        df = df.drop(labels=['snow', 'wdir', 'wpgt', 'pres', 'tsun'], axis=1)
+        list_data.append(df.reset_index())
+    
+    weather_data = pd.concat(list_data, axis=0)
+    iatas_repeated = [iata for iata in iatas for i in range(list_data[0].shape[0])]
+    weather_data['airport_code'] = iatas_repeated
+    return weather_data
 
 def merge_weather_data(X):
     X = X.copy()
